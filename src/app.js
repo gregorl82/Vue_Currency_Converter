bac83b2ee42e176aa717147797f2edf7e2bbf12a
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-12-17 18:37:46 +0000

    Adds getRates method to app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,20 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
-    data: {},
-    methods: {}
+    data: {
+      rates: {},
+    },
+    mounted() {
+      this.getRates()
+    },
+    methods: {
+      getRates: function(){
+        const request = fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then((data) => {
+          this.rates = data.rates
+        })
+      }
+    }
   })
 })
